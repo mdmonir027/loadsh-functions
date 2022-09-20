e53c2b3ec92930bc9d213226746d95e1dc2dad83
Monirul Islam <Monirul Islam>
2022-09-20 21:49:31 +0600

    code optimize drops function

--- array/drop.js
+++ array/drop.js
@@ -1,5 +1,13 @@
 function drop(arr, n = 1) {
-  return arr.slice(n);
+  const data = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (i + 1 > n) {
+      data.push(arr[i]);
+    }
+  }
+
+  return data;
 }
 
-console.log(drop([1, 2, 3, 4, 5, 6, 7], 4));
+console.log(drop([1, 2, 3, 4, 5, 6, 7], 6));
